91e2cc551f0b2e7d0fcca088b2b63b85a8371eeb
pampado <pampado@hotmail.com>
2020-04-23 03:35:27 -0400

    deploy

---
+++ src/utils/formatValue.ts
@@ -0,0 +1,9 @@
+const formatValue = (value: number): string =>
+  Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(
+    value,
+  )
+
+export const formatDate = (value: Date): string =>
+  Intl.DateTimeFormat('pt-BR').format(value)
+
+export default formatValue
